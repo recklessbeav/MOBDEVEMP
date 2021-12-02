37c0ebdb82b4e4840cf5975bb2aefc9a9fd134d5
Beaverly Cuasi <61379865+recklessbeav@users.noreply.github.com>
2021-12-03 03:18:20 +0800

    new files

--- app/src/main/java/com/mobdeve/s12/salamante/cuasi/beaverly/moodtracker/mobdeve/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/salamante/cuasi/beaverly/moodtracker/mobdeve/MainActivity.kt
@@ -2,10 +2,15 @@ package com.mobdeve.s12.salamante.cuasi.beaverly.moodtracker.mobdeve
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobdeve.s12.salamante.cuasi.beaverly.moodtracker.mobdeve.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
