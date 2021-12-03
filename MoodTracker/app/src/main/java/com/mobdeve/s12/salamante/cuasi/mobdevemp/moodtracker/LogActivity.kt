1eb11761d43de4ce5a61fb89a3c60e9050420dfe
Beaverly Cuasi <61379865+recklessbeav@users.noreply.github.com>
2021-12-03 13:14:52 +0800

    changed backend to mainly binding

--- MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/mobdevemp/moodtracker/LogActivity.kt
+++ MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/mobdevemp/moodtracker/LogActivity.kt
@@ -1,11 +1,33 @@
 package com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker.databinding.ActivityLogBinding
 
 class LogActivity : AppCompatActivity() {
+
+    var binding: ActivityLogBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log)
+        binding = ActivityLogBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+
+        var mood = intent.getStringExtra("mood")
+
+        binding!!.etLog.hint = "Why do you feel $mood?"
+
+        binding!!.btnReasonProceed.setOnClickListener {
+            val gotoWeekActivity = Intent(applicationContext, WeekActivity::class.java)
+            startActivity(gotoWeekActivity)
+            finish()
+        }
+
+
+
+
+
     }
 }
--- MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/mobdevemp/moodtracker/MainActivity.kt
+++ MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/mobdevemp/moodtracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker.databinding.ActivityMainBinding
@@ -12,5 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+
+        val gotoLogActivity = Intent(applicationContext, LogActivity::class.java)
+
+        binding!!.ivSad.setOnClickListener {
+            gotoLogActivity.putExtra("mood", binding!!.tvFeelingSad.text.toString())
+            startActivity(gotoLogActivity)
+        }
+
+        binding!!.ivHappy.setOnClickListener {
+            gotoLogActivity.putExtra("mood", binding!!.tvFeelingHappy.text.toString())
+            startActivity(gotoLogActivity)
+        }
+
+        binding!!.ivNeutral.setOnClickListener {
+            gotoLogActivity.putExtra("mood", binding!!.tvFeelingNeutral.text.toString())
+            startActivity(gotoLogActivity)
+        }
+
+        binding!!.btnLoggedAlready.setOnClickListener {
+            val gotoWeekActivity = Intent(applicationContext, WeekActivity::class.java)
+            startActivity(gotoWeekActivity)
+            finish()
+        }
+
+
     }
 }
