5a05ba292e74b0260b7e70c86d246834a8f09331
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2022-02-11 16:46:42 +0800

    Added needed data to show in Day Activity

--- MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/mobdevemp/moodtracker/DayActivity.kt
+++ MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/mobdevemp/moodtracker/DayActivity.kt
@@ -3,19 +3,36 @@ package com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker.databinding.ActivityDayBinding
+import com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker.util.SharedPrefUtility
 import java.text.SimpleDateFormat
 
 class DayActivity : AppCompatActivity() {
 
+    private lateinit var sharedPref: SharedPrefUtility
     var binding: ActivityDayBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initPrefs()
         binding = ActivityDayBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
+        var mood = sharedPref.getString("mood")
         var date = intent.getStringExtra("date")
 
-        binding!!.tvContent.text = date
+        if (mood == "Sad") {
+            binding!!.ivDay.setImageResource(R.drawable.sad_face)
+        } else if (mood == "Neutral") {
+            binding!!.ivDay.setImageResource(R.drawable.neutral_face)
+        } else {
+            binding!!.ivDay.setImageResource(R.drawable.happy_face)
+        }
+
+        binding!!.tvDayDate.text = date
+        binding!!.tvDayContent.text = sharedPref.getString("reason")
+    }
+
+    fun initPrefs() {
+        sharedPref = SharedPrefUtility(this)
     }
 }
