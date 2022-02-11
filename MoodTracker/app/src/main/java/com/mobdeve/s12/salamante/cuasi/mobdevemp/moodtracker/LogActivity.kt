a7dde57bd856d143460231a296262c97a6cc0640
Beaverly Cuasi <61379865+recklessbeav@users.noreply.github.com>
2022-02-11 19:10:01 +0800

    location

--- MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/mobdevemp/moodtracker/LogActivity.kt
+++ MoodTracker/app/src/main/java/com/mobdeve/s12/salamante/cuasi/mobdevemp/moodtracker/LogActivity.kt
@@ -1,13 +1,42 @@
 package com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker
 
+import android.Manifest
+import android.R
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+<<<<<<< Updated upstream
 import android.widget.Toast
+=======
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+>>>>>>> Stashed changes
 import com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker.databinding.ActivityLogBinding
 import com.mobdeve.s12.salamante.cuasi.mobdevemp.moodtracker.util.SharedPrefUtility
 
-class LogActivity : AppCompatActivity() {
+class LogActivity : AppCompatActivity(), LocationListener{
+
+    protected var locationManager: LocationManager? = null
+    protected var locationListener: LocationListener? = null
+    protected var context: Context? = null
+
+
+    var provider: String? = ""
+    var lat: String? = ""
+    protected var latitude: String? = ""
+    protected var longitude: String? = ""
+    protected var gps_enabled: Boolean? = false
+    protected var network_enabled: Boolean? = false
+
 
     private lateinit var sharedPref: SharedPrefUtility
     var binding: ActivityLogBinding? = null
@@ -38,9 +67,53 @@ class LogActivity : AppCompatActivity() {
             val gotoMapsActivity = Intent(applicationContext, MapsActivity::class.java)
             startActivity(gotoMapsActivity)
         }
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION )
+            == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
+        } else{
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)){
+                Toast.makeText(applicationContext, "Wrong Password",
+                    Toast.LENGTH_SHORT).show()
+            }else{
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+            }
+        }
+//        locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
+
+
+
     }
 
+<<<<<<< Updated upstream
     fun initPrefs() {
         sharedPref = SharedPrefUtility(this)
     }
+=======
+    override fun onLocationChanged(location: Location) {
+        binding!!.tvLoc.text = "Latitude:" + location.latitude + ", Longitude:" + location.longitude
+    }
+
+    override fun onProviderDisabled(provider: String) {
+        Log.d("Latitude", "disable")
+    }
+
+    override fun onProviderEnabled(provider: String) {
+        Log.d("Latitude", "enable")
+    }
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+        Log.d("Latitude", "status")
+    }
+
+
+>>>>>>> Stashed changes
 }
